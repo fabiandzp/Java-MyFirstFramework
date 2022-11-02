870cc949fbb6da7264f205b08e7a019d59cc2ade
fzabala <S3l3nium>
2022-11-02 16:12:21 -0500

    Fisrt commit

---
+++ src/test/java/com/framework/MyFristTest.java
@@ -0,0 +1,21 @@
+package com.framework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFristTest {
+
+
+    @Test
+    void myFirstTest () {
+        int result = sum(2, 2);
+        Assertions.assertEquals(4, result);
+
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+
+}
