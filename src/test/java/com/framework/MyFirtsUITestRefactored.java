c8f03bad9279bdf58d18a4ca962b6b205c49ca18
fzabala <S3l3nium>
2022-11-03 22:07:44 -0500

    Refactor - MyFirtsUITestRefactored

--- src/test/java/com/framework/MyFirtsUITestRefactored.java
+++ src/test/java/com/framework/MyFirtsUITestRefactored.java
@@ -1,15 +1,15 @@
 package com.framework;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
+import static com.testautomation.DriverFactory.getChromeDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyFirtsUITestRefactored {
@@ -21,12 +21,10 @@ public class MyFirtsUITestRefactored {
     //@BeforeEach // In TestNG it's called @BeforeMethod
     @BeforeAll
     static void setup(){
-        // Create the driver object
-        System.setProperty("webdriver.chrome.driver", "C:\\Projects\\PluralSight\\MyFirstFramework\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions().addArguments("start-fullscreen");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver = getChromeDriver();
     }
+
+
 
     //@AfterEach
     @AfterAll
