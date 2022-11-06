3343d2f7d0db11f4fac2b98e11336c3ba139374b
fzabala <S3l3nium>
2022-11-05 21:31:12 -0500

    Adding test to verify specific response Headers values

--- src/test/java/com/framework/javahttpclient/Java11GetHeaderTest.java
+++ src/test/java/com/framework/javahttpclient/Java11GetHeaderTest.java
@@ -20,18 +20,54 @@ public class Java11GetHeaderTest {
         HttpClient httpClient = HttpClient.newBuilder().build();
 
         HttpRequest get = HttpRequest.newBuilder(URI.create(BASE_URL))
-                .GET()
+                //.GET()
                 .setHeader("User-Agent", "Java 11 Http bot")
                 .build();
 
         // Act
         HttpResponse<Void> response = httpClient.send(get, HttpResponse.BodyHandlers.discarding());
         int actualCode = response.statusCode();
+//        String body = response.body().toString();
+//        System.out.println(body);
 
         // Assert
         Assertions.assertEquals(200, actualCode);
+    }
 
+    @Test
+    void contentTypeIsJson() throws IOException, InterruptedException {
+        // Arrange
+        HttpClient httpClient = HttpClient.newBuilder().build();
 
+        HttpRequest get = HttpRequest.newBuilder(URI.create(BASE_URL))
+                //.GET()
+                .setHeader("User-Agent", "Java 11 Http bot")
+                .build();
+
+        // Act
+        HttpResponse<Void> response = httpClient.send(get, HttpResponse.BodyHandlers.discarding());
+        String contentType = response.headers().firstValue("content-type").get();
+
+        // Assert
+        Assertions.assertEquals("application/json; charset=utf-8", contentType);
+    }
+
+    @Test
+    void XrATElIMITiSpRESENT() throws IOException, InterruptedException {
+        // Arrange
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        HttpRequest get = HttpRequest.newBuilder(URI.create(BASE_URL))
+                //.GET()
+                .setHeader("User-Agent", "Java 11 Http bot")
+                .build();
+
+        // Act
+        HttpResponse<Void> response = httpClient.send(get, HttpResponse.BodyHandlers.discarding());
+        String xRateLimit = response.headers().firstValue("x-ratelimit-limit").get();
+
+        // Assert
+        Assertions.assertEquals(60, Integer.parseInt(xRateLimit));
     }
 
 
